9f10aca8ca4dd114483d2091cede8b4113c5e472
CaoThanhLong <thanhlong2803@hotmail.com>
2022-04-14 22:56:01 +0700

    create controller

---
+++ be.internal-department-transfer-letter/be.internal-department-transfer-letter/Controllers/SettingTransferLetterController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace be.internal_department_transfer_letter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SettingTransferLetterController : ControllerBase
+    {
+       
+    }
+}
