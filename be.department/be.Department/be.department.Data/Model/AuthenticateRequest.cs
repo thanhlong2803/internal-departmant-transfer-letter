8a1db7b6290cf8aba4b25b673af6d2a90c5a824a
CaoThanhLong <thanhlong2803@hotmail.com>
2022-04-19 01:30:35 +0700

    add model authentication

---
+++ be.department/be.Department/be.department.Data/Model/AuthenticateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace be.Department.Models
+{
+    public class AuthenticateRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ be.department/be.Department/be.department.Data/Model/AuthenticateResponse.cs
@@ -0,0 +1,14 @@
+﻿using be.Data;
+
+namespace be.Department.Models
+{
+    public class AuthenticateResponse
+    {
+        public long Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public Role Role { get; set; }
+        public string Token { get; set; }
+    }
+}
