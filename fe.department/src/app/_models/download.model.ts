5787b9f6cd6aa20bc39bfbe00a1c0952203324e6
David Cao <thanhlong2803@hotmail.com>
2022-04-18 19:05:57 +0700

    delete old department

--- fe.department/src/app/_models/download.model.ts
+++
@@ -1,9 +0,0 @@
-
-export class DownLoad {
-	constructor(
-        public id: number = null,
-        public name: string = null,
-        public extension: string = null,
-        public type : string = null		
-	) { }
-}
