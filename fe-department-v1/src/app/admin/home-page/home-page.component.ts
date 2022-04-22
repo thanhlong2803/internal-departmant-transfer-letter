d2375728fb01c8f33c6d18556c8856a3fd40deed
CaoThanhLong <thanhlong2803@hotmail.com>
2022-04-22 22:50:05 +0700

    role per employee

--- fe-department-v1/src/app/admin/home-page/home-page.component.ts
+++ fe-department-v1/src/app/admin/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { first } from 'rxjs/operators';
-import { Employee, EmployeeList } from 'src/app/fe.data/employee/employee.model';
+import { Employee, EmployeeList, PositionType } from 'src/app/fe.data/employee/employee.model';
 import { AlertService } from 'src/app/fe.services/alert.service';
 import { AuthenticationService } from 'src/app/fe.services/authentication/authentication.service';
 import { EmployeeService } from 'src/app/fe.services/employee/employee.service';
@@ -16,6 +16,9 @@ export class HomePageComponent {
     currentUser: any;
     public manageEmployee = {} as Employee;
     public employeeList: Array<EmployeeList> = [];
+
+    admin: number = PositionType.Admin;
+    nember: number = PositionType.Nember;
 
     constructor(
         private alertService: AlertService,
