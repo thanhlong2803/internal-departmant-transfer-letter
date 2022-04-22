fb880fe807bbacfa6b28ac9516efa0d3ea2bb3ba
CaoThanhLong <thanhlong2803@hotmail.com>
2022-04-22 22:49:36 +0700

    change a bit AuthenticateResponse

--- be.department/be.Department/be.department.Data/Model/AuthenticateResponse.cs
+++ be.department/be.Department/be.department.Data/Model/AuthenticateResponse.cs
@@ -4,13 +4,15 @@ namespace be.Data
 {
     public class AuthenticateResponse
     {
-        public long Id { get; set; }       
+        public long Id { get; set; }
+        public string? FullName { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Email { get; set; }
         public Role Role { get; set; }
         public string Token { get; set; }
-
+        public PositionType PostionId { get; set; }
+        public string PostionName => PostionId.GetDescription();
         public long DepartmentId { get; set; }
         public string? DepartmentName { get; set; }
     }
